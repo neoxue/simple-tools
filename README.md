38cb4eec23fc7092f37642d378f549476ecfbd46
xue <xuefenglei04@126.com>
2017-10-21 22:29:51 +0800

    up

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # simple-tools
 some simple tools
+
+done:
+add jpr
+*   jpr ( jsonlog prettify shell tool)
+    : add color and replace json's \n \t to real \n \t
+        
+
+# TODO
+    
+    
---
+++ jpr.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"encoding/json"
+	. "github.com/logrusorgru/aurora"
+)
+/*
+https://stackoverflow.com/questions/43843477/scanln-in-golang-doesnt-accept-whitespace
+fmt.scanln does not accept white space
+instead, use bufio.NewScanner
+
+ */
+
+//str,_ := jsonpath.JsonPathLookup(jsonObject, "$.msg")
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		line := scanner.Text()
+		var jsonObject interface{}
+		json.Unmarshal([]byte(line), &jsonObject)
+		recursivePrint2(jsonObject, 0, "\n")
+	}
+}
+
+func recursivePrint2(data interface{}, n int, tail string) {
+	switch data.(type) {
+		default:
+			jsonStr, _ :=json.MarshalIndent(data, "", "  ")
+			fmt.Printf(`"%v"`, Red(string(jsonStr)))
+		case bool:
+			fmt.Printf(`"%v"`, Brown(data))
+		case int:
+			fmt.Printf(`"%v"`, Gray(data))
+		case string:
+			fmt.Printf(`"%v"`, Magenta(data.(string)))
+		case map[string] interface{}:
+			recursivePrintWhiteSpace(n)
+			fmt.Println("{")
+			dataArr := data.(map[string] interface{})
+			for k2, v2 := range dataArr {
+				recursivePrintWhiteSpace(n + 1)
+				fmt.Printf(`"%v":`, Green(k2))
+				recursivePrint2(v2, n + 1, ",\n")
+			}
+			recursivePrintWhiteSpace(n + 1)
+			fmt.Print("}")
+		case [] interface{}:
+			fmt.Println("[")
+			dataArr := data.([]interface{})
+			for _, v2 := range dataArr {
+				recursivePrintWhiteSpace(n + 1)
+				recursivePrint2(v2, n + 1, ",\n")
+			}
+			recursivePrintWhiteSpace(n)
+			fmt.Print("]")
+	}
+	fmt.Print(tail)
+}
+func recursivePrintWhiteSpace(n int) {
+	for i := 0; i<n; i++ {
+		fmt.Print("  ")
+	}
+}
+
+
+
+
